79cb6021e9e600280c26837d32a6d567610dfb71
Jorenvdb1996 <75526366+Jorenvdb1996@users.noreply.github.com>
2022-05-11 23:16:42 +0200

    test

---
+++ src/main/java/graduaten/hethalfkieke2/HetHalfKieke2Application.java
@@ -0,0 +1,13 @@
+package graduaten.hethalfkieke2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HetHalfKieke2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HetHalfKieke2Application.class, args);
+    }
+
+}
---
+++ src/test/java/graduaten/hethalfkieke2/HetHalfKieke2ApplicationTests.java
@@ -0,0 +1,13 @@
+package graduaten.hethalfkieke2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HetHalfKieke2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
